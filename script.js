c11f1124b5a8dd2336376275e6c1d4b22f21ef8c
balibek <66714946+balibek@users.noreply.github.com>
2020-10-19 17:34:23 +0600

    2var cycle

--- script.js
+++ script.js
@@ -84,4 +84,30 @@ if (personalMovieDB.count < 10) {
     console.log("Произошла ошибка");
 }
 
+let i = 0;
+while (i < 2) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?','');
+  i++;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+} else {
+    console.log('error');
+    i--;
+}
+}
 
+
+let i = 0;
+do {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?','');
+  i++;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+} else {
+    console.log('error');
+    i--;}
+} while (i < 2);
